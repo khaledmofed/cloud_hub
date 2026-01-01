c656d4cca923352c97ee0eb17ab9bc5d85d5cdd6
khaled mofed <khaled.mofed1@gmail.com>
2026-01-01 17:09:58 +0200

    update menu

--- components/MobileMenu/MobileMenu.js
+++ components/MobileMenu/MobileMenu.js
@@ -1,109 +1,136 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState } from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/List";
 import Collapse from "@mui/material/Collapse";
-import Link from 'next/link'
+import Link from "next/link";
 
 const menus = [
-    {
-        id: 1,
-        title: 'Home',
-        link: '/',
-    },
-    {
-        id: 3,
-        title: 'Pricing',
-        link: '/pricing',
-    },
-    {
-        id: 4,
-        title: 'Service',
-        link: '/service',
-        submenu: [
-            {
-                id: 41,
-                title: 'Service',
-                link: '/service'
-            },
-            {
-                id: 42,
-                title: 'Service Details',
-                link: '/service-single/IT-Management-Services'
-            }
-        ]
-    },
-    {
-        id: 5,
-        title: 'Portfolio',
-        link: '/portfolio',
-    },
-    {
-        id: 6,
-        title: 'Blog',
-        link: '/blog',
-        submenu: [
-            {
-                id: 61,
-                title: 'Blog',
-                link: '/blog'
-            },
-            {
-                id: 62,
-                title: 'Blog Details',
-                link: '/blog-single/How-Our-Software-Solutions-Drive-Insights.'
-            },
-        ]
-    },
-    {
-        id: 88,
-        title: 'Contact',
-        link: '/contact',
-    }
-
-
-]
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    title: "About Us",
+    link: "/about",
+  },
+  {
+    id: 3,
+    title: "Portfolio",
+    link: "/portfolio",
+  },
+  {
+    id: 4,
+    title: "Services",
+    link: "/service",
+    submenu: [
+      {
+        id: 41,
+        title: "All Services",
+        link: "/service",
+      },
+      {
+        id: 42,
+        title: "IT Management Services",
+        link: "/service-single/IT-Management-Services",
+      },
+      {
+        id: 43,
+        title: "Data Tracking Security",
+        link: "/service-single/Data-Tracking-and-Security",
+      },
+      {
+        id: 44,
+        title: "Website Development",
+        link: "/service-single/IT-Management-Services",
+      },
+      {
+        id: 45,
+        title: "CRM Solutions and Design",
+        link: "/service-single/IT-Management-Services",
+      },
+      {
+        id: 46,
+        title: "UI/UX Design Services",
+        link: "/service-single/IT-Management-Services",
+      },
+      {
+        id: 47,
+        title: "Technology Solution",
+        link: "/service-single/IT-Management-Services",
+      },
+      {
+        id: 48,
+        title: "Software Development",
+        link: "/service-single/IT-Management-Services",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Pricing",
+    link: "/pricing",
+  },
+  {
+    id: 6,
+    title: "Contact",
+    link: "/contact",
+  },
+];
 
 const MobileMenu = () => {
+  const [openId, setOpenId] = useState(0);
 
-    const [openId, setOpenId] = useState(0);
+  const ClickHandler = () => {
+    window.scrollTo(10, 0);
+  };
 
-    const ClickHandler = () => {
-        window.scrollTo(10, 0);
-    }
-
-    return (
-        <ul className="main_menu_list clearfix">
-            {menus.map((item, mn) => {
-                return (
-                    <ListItem className={item.id === openId ? 'active' : null} key={mn}>
-                        {item.submenu ?
-                            <Fragment>
-                                <p onClick={() => setOpenId(item.id === openId ? 0 : item.id)}>{item.title}
-                                    <i className={item.id === openId ? 'fa fa-angle-up' : 'fa fa-angle-down'}></i>
-                                </p>
-                                <Collapse in={item.id === openId} timeout="auto" unmountOnExit>
-                                    <List className="subMenu">
-                                        <Fragment>
-                                            {item.submenu.map((submenu, i) => {
-                                                return (
-                                                    <ListItem key={i}>
-                                                        <Link onClick={ClickHandler} className="active"
-                                                            href={submenu.link}>{submenu.title}</Link>
-                                                    </ListItem>
-                                                )
-                                            })}
-                                        </Fragment>
-                                    </List>
-                                </Collapse>
-                            </Fragment>
-                            : <Link className="active"
-                                href={item.link}>{item.title}</Link>
-                        }
-                    </ListItem>
-                )
-            })}
-        </ul>
-    )
-}
+  return (
+    <ul className="main_menu_list clearfix">
+      {menus.map((item, mn) => {
+        return (
+          <ListItem className={item.id === openId ? "active" : null} key={mn}>
+            {item.submenu ? (
+              <Fragment>
+                <p onClick={() => setOpenId(item.id === openId ? 0 : item.id)}>
+                  {item.title}
+                  <i
+                    className={
+                      item.id === openId ? "fa fa-angle-up" : "fa fa-angle-down"
+                    }
+                  ></i>
+                </p>
+                <Collapse in={item.id === openId} timeout="auto" unmountOnExit>
+                  <List className="subMenu">
+                    <Fragment>
+                      {item.submenu.map((submenu, i) => {
+                        return (
+                          <ListItem key={i}>
+                            <Link
+                              onClick={ClickHandler}
+                              className="active"
+                              href={submenu.link}
+                            >
+                              {submenu.title}
+                            </Link>
+                          </ListItem>
+                        );
+                      })}
+                    </Fragment>
+                  </List>
+                </Collapse>
+              </Fragment>
+            ) : (
+              <Link className="active" href={item.link}>
+                {item.title}
+              </Link>
+            )}
+          </ListItem>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default MobileMenu;
